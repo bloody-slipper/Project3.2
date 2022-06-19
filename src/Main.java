ee5b5a7199e9859edead87784436933582fb1160
bloody-slipper <anniershova@gmail.com>
2022-06-19 15:23:11 +0300

    delete out

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
+        double balance = sc.nextDouble();
+        double payment = sc.nextDouble();
         int bonus;
-        if (b>1000){
-            bonus = (int) b / 100;
+        if (payment > 1000) {
+            bonus = (int) payment / 100;
         } else {
             bonus = 0;
         }
-        System.out.println(a + b + bonus);
+        System.out.println(balance + payment + bonus);
         System.out.println(bonus);
     }
 }
